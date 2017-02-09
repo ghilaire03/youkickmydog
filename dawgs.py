6d47d48d1ede63b5fe702beda55180c3d649f16f
Greg <ghilaire03@gmail.com>
2017-02-08 19:27:17 -0500

    Add files via upload

---
+++ dawgs.py
@@ -0,0 +1,3 @@
+age=float(input("How old is your cuddly companion? "))
+a= (age)*7
+print("Your dog is really ", a)
---
+++ output.py
@@ -0,0 +1,5 @@
+number=int(input("Pick a number " ))
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
