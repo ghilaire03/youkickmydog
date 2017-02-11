20189df3597e334d3b60a1d562163976578fa56a
Greg <ghilaire03@gmail.com>
2017-02-11 14:03:23 -0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,6 @@
+def main():
+    b = int(input("How many degrees fahrenheit today? "))
+    sum = 0
+    print("temperature today in celsius is",(b-32)*5/9)
+
+main()
---
+++ temprange.py
@@ -0,0 +1,5 @@
+def main():
+    for celsius in range(0,105,5):
+        print (celsius," celsius")
+        print(celsius-32*5/9," fahrenheit")
+main()
