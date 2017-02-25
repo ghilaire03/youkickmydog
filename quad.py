b11147b8e50f8a309cd0e08b4907d365b22a2032
Greg <ghilaire03@gmail.com>
2017-02-25 13:26:14 -0500

    Add files via upload
    quaaad

--- quad.py
+++ quad.py
@@ -1,22 +1,8 @@
-# Solve the quadratic equation ax**2 + bx + c = 0
+a=int(input("what is the value of a?  "))
+b=int(input("what is the value of b? "))
+c=int(input("what is the value of c? "))
+d=int(input("what is the value of x?"))
 
-# import complex math module
-import cmath
-
-a = 1
-b = 5
-c = 6
-
-# To take coefficient input from the users
-# a = float(input('Enter a: '))
-# b = float(input('Enter b: '))
-# c = float(input('Enter c: '))
-
-# calculate the discriminant
-d = (b**2) - (4*a*c)
-
-# find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
-
-print('The solution are {0} and {1}'.format(sol1,sol2))
+print("The following quadratic was entered:",a,"X^2",+b,"X+",c)
+solve= (a*(d**2))+(b*d)+c
+print("The result is",solve)
